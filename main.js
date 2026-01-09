8b0b2273fe9d5709cbf993efd65fda545bdd5400
Cuevasaj <92329439+Cuevasaj@users.noreply.github.com>
2026-01-08 17:13:51 -0700

    math functions added and some functionality with UI

--- main.js
+++ main.js
@@ -14,8 +14,48 @@ const divide = (num1, num2) => {
   return num1 / num2;
 };
 
-const operate = (num1, operator, num2) => {
-  let num1 = 0;
-  let num2 = 0;
-  let operate;
+const operate = (leftNum, operator, rightNum) => {
+  if (operator === "+") {
+    return add(leftNum, rightNum);
+  } else if (operator === "-") {
+    return subtract(leftNum, rightNum);
+  } else if (operator === "*") {
+    multiply(leftNum, rightNum);
+  } else if (operator === "/") {
+    return divide(leftNum, rightNum);
+  }
 };
+
+const currentOperand = document.querySelector(".current-operand");
+
+const numBtn = document.querySelectorAll(".btn-number");
+numBtn.forEach((button) => {
+  button.addEventListener("click", function (event) {
+    currentOperand.textContent += event.target.textContent;
+    console.log(`You clicked number: ${event.target.textContent}`);
+  });
+});
+
+const btnOperator = document.querySelectorAll(".btn-operator");
+btnOperator.forEach((button) => {
+  button.addEventListener("click", function (event) {
+    console.log(`You clicked Operator: ${event.target.textContent}`);
+  });
+});
+
+const btnEquals = document.querySelector(".btn-equals");
+console.log(btnEquals);
+btnEquals.addEventListener("click", function (event) {
+  console.log(`You clicked: ${event.target.textContent}`);
+});
+
+const btnClear = document.querySelector(".btn-clear");
+btnClear.addEventListener("click", function (event) {
+  currentOperand.textContent = "";
+  console.log(`You clicked ${event.target.textContent}`);
+});
+
+const btnDel = document.querySelector(".btn-del");
+btnDel.addEventListener("click", function (event) {
+  console.log(`You clicked ${event.target.textContent}`);
+});
